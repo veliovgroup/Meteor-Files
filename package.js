3497d6458c3f60801bed90cb6dae7a6a66b62d72
Dmitriy A. Golev <dmitriygolev@Dmitriys-MacBook-Pro.local>
2015-11-10 14:27:48 +0300

    v1.3.14
    - Get rid of iron-router in favour of native web-hooks
    - Fix issue when user object is unavailable in protected function, see
    #48
    - Overall stability improvements, for usage behind nginx

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:files',
-  version: '1.3.13',
+  version: '1.3.14',
   summary: 'Upload, Store and Stream (Video & Audio streaming) files to/from file system (FS) via DDP and HTTP',
   git: 'https://github.com/VeliovGroup/Meteor-Files',
   documentation: 'README.md'
@@ -9,8 +9,9 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.1');
   api.addFiles('files.coffee');
+  api.use('webapp', 'server');
   api.use(['templating', 'reactive-var', 'tracker'], 'client');
-  api.use(['underscore', 'check', 'sha', 'ostrio:cookies@2.0.1', 'random', 'coffeescript', 'iron:router@1.0.12', 'aldeed:collection2@2.5.0'], ['client', 'server']);
+  api.use(['underscore', 'check', 'sha', 'ostrio:cookies@2.0.1', 'random', 'coffeescript', 'aldeed:collection2@2.5.0'], ['client', 'server']);
 });
 
 Npm.depends({
