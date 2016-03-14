a64ad41269136abff7e884f322b8a0eb46f31665
Dmitriy A. Golev <ceo@veliov.com>
2016-03-14 20:00:40 +0300

    v1.3.16
     - Update external dependencies
     - Minor enhancements in demo app

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:files',
-  version: '1.3.15',
+  version: '1.3.16',
   summary: 'Upload, Store and Stream (Video & Audio streaming) files to/from file system (FS) via DDP and HTTP',
   git: 'https://github.com/VeliovGroup/Meteor-Files',
   documentation: 'README.md'
@@ -11,11 +11,11 @@ Package.onUse(function(api) {
   api.addFiles('files.coffee');
   api.use('webapp', 'server');
   api.use(['templating', 'reactive-var', 'tracker'], 'client');
-  api.use(['underscore', 'check', 'sha', 'ostrio:cookies@2.0.1', 'random', 'coffeescript', 'aldeed:collection2@2.5.0'], ['client', 'server']);
+  api.use(['underscore', 'check', 'sha', 'ostrio:cookies@2.0.2', 'random', 'coffeescript', 'aldeed:collection2@2.9.0'], ['client', 'server']);
 });
 
 Npm.depends({
-  'fs-extra': '0.22.1',
-  'request': '2.58.0',
+  'fs-extra': '0.26.5',
+  'request': '2.69.0',
   'throttle': '1.0.3'
 });
