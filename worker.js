8920b74b1564eb90bd6579b50f8d8292569343ea
Dmitriy A. Golev <ceo@veliov.com>
2016-04-25 01:55:03 +0300

    Support for FireFox Mozilla < 46
    See Mozilla bug: https://bugzilla.mozilla.org/show_bug.cgi?id=901097

--- worker.js
+++ worker.js
@@ -1,14 +1,22 @@
 self.onmessage = function(e) {
-  var fileReader = new FileReader();
+  if (self.FileReader) {
+    var fileReader = new FileReader();
+    fileReader.onloadend = function(chunk) {
+      postMessage({bin: (fileReader.result || chunk.srcElement || chunk.target).split(',')[1], chunkId: e.data.currentChunk, start: e.data.start});
+    };
 
-  fileReader.onloadend = function(chunk) {
-    postMessage({bin: (fileReader.result || chunk.srcElement || chunk.target).split(',')[1], chunkId: e.data.currentChunk, start: e.data.start});
-  };
+    fileReader.onerror = function(error) {
+      throw (error.target || error.srcElement).error;
+    };
 
-  fileReader.onerror = function(error) {
-    throw (error.target || error.srcElement).error;
-  };
+    fileReader.readAsDataURL(e.data.file.slice(e.data.chunkSize * (e.data.currentChunk - 1), e.data.chunkSize * e.data.currentChunk));
 
-  fileReader.readAsDataURL(e.data.file.slice(e.data.chunkSize * (e.data.currentChunk - 1), e.data.chunkSize * e.data.currentChunk));
+  } else if (self.FileReaderSync) {
+    var fileReader = new FileReaderSync();
+    postMessage({bin: fileReader.readAsDataURL(e.data.file.slice(e.data.chunkSize * (e.data.currentChunk - 1), e.data.chunkSize * e.data.currentChunk)).split(',')[1], chunkId: e.data.currentChunk, start: e.data.start});
+
+  } else {
+    postMessage({bin: null, chunkId: e.data.currentChunk, start: e.data.start, error: 'FileReader and fileReaderSunc is not supported in WebWorker!'});
+  }
   return;
 };
