a62db522ee8d8fbe4e31cb9d891e4d272eb243db
Dmitriy A. Golev <ceo@veliov.com>
2016-04-27 04:12:51 +0300

    Update to v1.5.0.
     - Overall enhancements
     - Remove unused and empty files

--- demo-simplest-download-button/lib/images.collection.js
+++ demo-simplest-download-button/lib/images.collection.js
@@ -2,16 +2,23 @@ this.Images = new Meteor.Files({collectionName: 'Images'});
 
 // To have sample image in DB we will upload it on server startup:
 if (Meteor.isServer) {
+  Images.denyClient();
+  Images.collection.attachSchema(Images.schema);
+
   Meteor.startup(function () {
-    Images.load('https://raw.githubusercontent.com/VeliovGroup/Meteor-Files/master/logo.png', {
-      fileName: 'logo.png',
-      meta: {}
-    });
+    if (!Images.collection.findOne({})) {
+      Images.load('https://raw.githubusercontent.com/VeliovGroup/Meteor-Files/master/logo.png', {
+        fileName: 'logo.png',
+        meta: {}
+      });
+    }
   });
 
   Meteor.publish('files.images.all', function () {
     return Images.collection.find({});
   });
+
 } else {
+
   Meteor.subscribe('files.images.all');
 }
