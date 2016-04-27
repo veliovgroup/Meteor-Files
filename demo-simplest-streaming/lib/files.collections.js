c97b5520d4c8014d6093b3ac1838aa1dbd8448fe
Dmitriy A. Golev <ceo@veliov.com>
2016-04-27 04:15:37 +0300

    Update to v1.5.0
     - Overall enhancements
     - Remove unused and empty files

--- demo-simplest-streaming/lib/files.collections.js
+++ demo-simplest-streaming/lib/files.collections.js
@@ -5,6 +5,7 @@ this.Images = new Meteor.Files({
     return false;
   }
 });
+
 this.Videos = new Meteor.Files({
   collectionName: 'Videos',
   onBeforeUpload: function () {
@@ -15,23 +16,33 @@ this.Videos = new Meteor.Files({
 
 // To have sample files in DB we will upload them on server startup:
 if (Meteor.isServer) {
+  Images.denyClient();
+  Videos.denyClient();
+  
   Meteor.startup(function () {
-    Images.load('https://raw.githubusercontent.com/VeliovGroup/Meteor-Files/master/logo.png', {
-      fileName: 'logo.png'
-    });
-    Videos.load('http://www.sample-videos.com/video/mp4/240/big_buck_bunny_240p_5mb.mp4', {
-      fileName: 'Big-Buck-Bunny.mp4'
-    });
+    if (!Images.collection.findOne({})) {
+      Images.load('https://raw.githubusercontent.com/VeliovGroup/Meteor-Files/master/logo.png', {
+        fileName: 'logo.png'
+      });
+    }
+
+    if (!Videos.collection.findOne({})) {
+      Videos.load('http://www.sample-videos.com/video/mp4/240/big_buck_bunny_240p_5mb.mp4', {
+        fileName: 'Big-Buck-Bunny.mp4'
+      });
+    }
   });
 
   Meteor.publish('files.images.all', function () {
     return Images.collection.find({});
   });
+
   Meteor.publish('files.videos.all', function () {
     return Videos.collection.find({});
   });
 
 } else {
+
   Meteor.subscribe('files.images.all');
   Meteor.subscribe('files.videos.all');
 }
--- demo-simplest-streaming/server/main.js
+++
@@ -1,5 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-
-Meteor.startup(() => {
-  // code to run on server at startup
-});
