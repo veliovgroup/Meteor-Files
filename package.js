28ab741c9b3c477f94209546298f6329a795660f
Dmitriy A. Golev <ceo@veliov.com>
2016-08-16 04:21:11 +0300

    v1.6.10
     - Fix #183 (Use more certain mime-type check)
     - Fix #186 (Merge #187, remove query-string from file's extension)
     - Fix #188 (Use `Meteor.absoluteUrl` to get WebWorker)
     - File extension now always is lowercase
     - Avoid `JSON.stringify` and `JSON.parse` for HTTP uploads (*1.5x
    upload speed improvement*)
     - WebWorker: strict mode
     - WebWorker: minor enhancements
     - Overall optimization
     - [DDP/HTTP upload difference
    wiki](https://github.com/VeliovGroup/Meteor-Files/wiki/About-Upload-Tran
    sports)

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:files',
-  version: '1.6.9',
+  version: '1.6.10',
   summary: 'Fast and robust file upload package, with support of FS, AWS, GridFS, DropBox or Google Drive',
   git: 'https://github.com/VeliovGroup/Meteor-Files',
   documentation: 'README.md'
