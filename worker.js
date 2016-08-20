7aff5b6a61e778aafac0fadfe632625450f42970
Dmitriy A. Golev <ceo@veliov.com>
2016-08-20 04:50:03 +0300

    Improved WebWorker + minified version

--- worker.js
+++ worker.js
@@ -4,21 +4,21 @@ self.onmessage = function(e) {
   if (self.FileReader) {
     fileReader = new FileReader();
     fileReader.onloadend = function(chunk) {
-      postMessage({bin: (fileReader.result || chunk.srcElement || chunk.target).split(',')[1], chunkId: e.data.currentChunk, start: e.data.start});
+      postMessage({bin: (fileReader.result || chunk.srcElement || chunk.target).split(',')[1], chunkId: e.data.cc, s: e.data.s});
     };
 
     fileReader.onerror = function(error) {
       throw (error.target || error.srcElement).error;
     };
 
-    fileReader.readAsDataURL(e.data.file.slice(e.data.chunkSize * (e.data.currentChunk - 1), e.data.chunkSize * e.data.currentChunk));
+    fileReader.readAsDataURL(e.data.f.slice(e.data.cs * (e.data.cc - 1), e.data.cs * e.data.cc));
 
   } else if (self.FileReaderSync) {
     fileReader = new FileReaderSync();
-    postMessage({bin: fileReader.readAsDataURL(e.data.file.slice(e.data.chunkSize * (e.data.currentChunk - 1), e.data.chunkSize * e.data.currentChunk)).split(',')[1], chunkId: e.data.currentChunk, start: e.data.start});
+    postMessage({bin: fileReader.readAsDataURL(e.data.f.slice(e.data.cs * (e.data.cc - 1), e.data.cs * e.data.cc)).split(',')[1], chunkId: e.data.cc});
 
   } else {
-    postMessage({bin: null, chunkId: e.data.currentChunk, start: e.data.start, error: 'FileReader and FileReaderSync is not supported in WebWorker!'});
+    postMessage({bin: null, chunkId: e.data.cc, error: 'File API is not supported in WebWorker!'});
   }
   return;
 };
