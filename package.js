bccbd1403aab73e1aff00403c568b72d48515cdc
Dmitriy A. Golev <ceo@veliov.com>
2016-08-20 04:58:48 +0300

    v1.7.0
     - Fix memory leak caused by unsatisfied queue in `writeStream` class
     - New authentication model for `http` uploads based on more secure and
    frequently changed `connectionId` see
    [#159](https://github.com/VeliovGroup/Meteor-Files/issues/159)
     - Fixed overwhelming requests to `Meteor.users` on every received chunk
     - Minimised amount of data transferred between WebWorker and MainThread
     - Use minified version of WebWorker
     - `http` upload uses HTTP requests for all tasks `start`, `continue`,
    `EOF`, previously Meteor methods was involved
     - Overall code enhancements, DRY

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'ostrio:files',
-  version: '1.6.10',
-  summary: 'Fast and robust file upload package, with support of FS, AWS, GridFS, DropBox or Google Drive',
+  version: '1.7.0',
+  summary: 'Fast and robust file upload package, with support of DDP, HTTP and WebRTC/DC uploads. Support of FS, AWS, GridFS, DropBox or Google Drive',
   git: 'https://github.com/VeliovGroup/Meteor-Files',
   documentation: 'README.md'
 });
@@ -12,7 +12,7 @@ Package.onUse(function(api) {
   api.use(['templating', 'reactive-var', 'tracker', 'http'], 'client');
   api.use(['mongo','underscore', 'check', 'random', 'coffeescript', 'ecmascript', 'ostrio:cookies@2.0.5'], ['client', 'server']);
   api.addFiles('event-emitter.jsx', 'client');
-  api.addAssets('worker.js', 'client');
+  api.addAssets('worker.min.js', 'client');
   api.mainModule('files.coffee', ['server', 'client']);
   api.export('FilesCollection');
 });
