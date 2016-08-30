086dc06b34ed66ac8a26c1538e3a6d3683a81307
Dmitriy A. Golev <ceo@veliov.com>
2016-08-30 16:39:02 +0300

    Update demo-simplest-upload

--- demo-simplest-upload/lib/images.collection.js
+++ demo-simplest-upload/lib/images.collection.js
@@ -4,7 +4,7 @@ this.Images = new Meteor.Files({
   allowClientCode: false, // Disallow remove files from Client
   onBeforeUpload: function (file) {
     // Allow upload files under 10MB, and only in png/jpg/jpeg formats
-    if (file.size <= 10485760 && /png|jpg|jpeg/i.test(file.extension)) {
+    if (file.size <= 1024*1024*10 && /png|jpg|jpeg/i.test(file.extension)) {
       return true;
     } else {
       return 'Please upload image, with size equal or less than 10MB';
