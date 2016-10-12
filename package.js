5ab13c1c756cca4050744c362557a86c72015a52
dr-dimitru <ceo@veliov.com>
2016-10-13 02:58:22 +0300

    Remove `templating` dependency

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:files',
-  version: '1.7.4',
+  version: '1.7.5',
   summary: 'Upload files via DDP, HTTP and WebRTC/DC. To server FS, AWS, GridFS, DropBox or Google Drive.',
   git: 'https://github.com/VeliovGroup/Meteor-Files',
   documentation: 'README.md'
@@ -9,7 +9,6 @@ Package.describe({
 Package.onUse(function(api) {
   api.versionsFrom('1.4');
   api.use('webapp', 'server');
-  api.use('templating', 'client', {weak: true});
   api.use(['reactive-var', 'tracker', 'http'], 'client');
   api.use(['mongo','underscore', 'check', 'random', 'coffeescript', 'ecmascript', 'ostrio:cookies@2.0.5'], ['client', 'server']);
   api.addFiles('event-emitter.jsx', 'client');
