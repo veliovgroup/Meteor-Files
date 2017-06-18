f9b3aba2b6328af86b3f2901ac1de58915f15624
dr.dimitru <ceo@veliov.com>
2017-06-18 13:35:35 +0300

    v1.8.0
     - Exact code splitting for Client and Server to avoid backend NPM
    package being imported on the Client. Fixing: #450 #445 #444
     - Catch Exception on `new Blob` and `window.URL.createObjectURL`
    although `Blob` and `URL` is exists on global scope may thrown an error
    on access in some bizarre browsers

--- package.js
+++ package.js
@@ -1,9 +1,16 @@
 Package.describe({
   name: 'ostrio:files',
-  version: '1.7.17',
+  version: '1.8.0',
   summary: 'File upload via DDP/HTTP to server FS, AWS, GridFS, DropBox, Google Drive or other 3rd party storage',
   git: 'https://github.com/VeliovGroup/Meteor-Files',
   documentation: 'README.md'
+});
+
+Npm.depends({
+  'fs-extra': '3.0.1',
+  'request': '2.81.0',
+  'throttle': '1.0.3',
+  'file-type': '5.2.0'
 });
 
 Package.onUse(function(api) {
@@ -12,13 +19,7 @@ Package.onUse(function(api) {
   api.use(['reactive-var', 'tracker', 'http'], 'client');
   api.use(['mongo', 'underscore', 'check', 'random', 'coffeescript', 'ecmascript', 'ostrio:cookies@2.2.1'], ['client', 'server']);
   api.addAssets('worker.min.js', 'client');
-  api.mainModule('files.coffee', ['server', 'client']);
+  api.mainModule('files-server.coffee', 'server');
+  api.mainModule('files-client.coffee', 'client');
   api.export('FilesCollection');
-});
-
-Npm.depends({
-  'fs-extra': '3.0.1',
-  'request': '2.81.0',
-  'throttle': '1.0.3',
-  'file-type': '4.3.0'
 });
