33ffafc0821121c9ad293c297d2bdb10a982c5db
dr.dimitru <ceo@veliov.com>
2017-10-19 11:25:28 +0300

    v1.9.0
     - Compatibility with `meteor@1.5.2.2`
     - NPM and Atmosphere dependencies update
     - No more coffee and CoffeeScript
     - __Deprecated__: globally available `Meteor.Files`
     - Implemented: `disableUpload` - Disable file upload, useful for
    server only solutions, see updated [*Constructor*
    docs](https://github.com/VeliovGroup/Meteor-Files/wiki/Constructor)
     - Implemented: `disableDownload` - Disable file download (serving),
    useful for file management only solutions, see updated [*Constructor*
    docs](https://github.com/VeliovGroup/Meteor-Files/wiki/Constructor)
     - Minor fix in [Google Cloud Storage
    Integration](https://github.com/VeliovGroup/Meteor-Files/wiki/Google-Clo
    ud-Storage-Integration), big thanks to @amos-whitewolf , see #498
     - Implemented: Custom headers in [`.load()` method
    (*updated*)](https://github.com/VeliovGroup/Meteor-Files/wiki/Load),
    thanks to @zwjcarter , see #501
     - Move events interface to [`eventemitter3`
    package](https://github.com/primus/eventemitter3)
     - Solve #500 removing 25ms delay before setting `auth-token-cookie`
    for `protected` downloads, thanks to @vbelolapotkov for pointing this
    out. Not sure if this will help for initial issue which stated `First
    download attempt`, as Meteor's login is async and happens only after
    DDP connection is established.
     - Fix `this.user` in `onBeforeRemove` hook
     - Deprecate (*temporarily*) `throttle` option, not sure if it was
    working from the beginning

--- package.js
+++ package.js
@@ -1,25 +1,25 @@
 Package.describe({
   name: 'ostrio:files',
-  version: '1.8.3',
+  version: '1.9.0',
   summary: 'File upload via DDP/HTTP to server FS, AWS, GridFS, DropBox, Google Drive or other 3rd party storage',
   git: 'https://github.com/VeliovGroup/Meteor-Files',
   documentation: 'README.md'
 });
 
 Npm.depends({
-  'fs-extra': '4.0.1',
-  'request': '2.81.0',
-  'throttle': '1.0.3',
-  'file-type': '6.1.0'
+  'fs-extra': '4.0.2',
+  'request': '2.83.0',
+  'file-type': '7.1.0',
+  'eventemitter3': '2.0.3'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.4');
   api.use('webapp', 'server');
   api.use(['reactive-var', 'tracker', 'http'], 'client');
-  api.use(['mongo', 'underscore', 'check', 'random', 'coffeescript', 'ecmascript', 'ostrio:cookies@2.2.2'], ['client', 'server']);
+  api.use(['mongo', 'underscore', 'check', 'random', 'ecmascript', 'ostrio:cookies@2.2.3'], ['client', 'server']);
   api.addAssets('worker.min.js', 'client');
-  api.mainModule('server.coffee', 'server');
-  api.mainModule('client.coffee', 'client');
+  api.mainModule('server.js', 'server');
+  api.mainModule('client.js', 'client');
   api.export('FilesCollection');
 });
