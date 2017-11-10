14db7ed731aa764423aa61d1209e314200573bb3
dr.dimitru <ceo@veliov.com>
2017-11-11 00:56:07 +0300

    v1.9.1
     - Dependencies update
     - Compatibility with `meteor@1.6`
     - Compatibility with `node@8.8.1`

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:files',
-  version: '1.9.0',
+  version: '1.9.1',
   summary: 'File upload via DDP/HTTP to server FS, AWS, GridFS, DropBox, Google Drive or other 3rd party storage',
   git: 'https://github.com/VeliovGroup/Meteor-Files',
   documentation: 'README.md'
@@ -9,7 +9,7 @@ Package.describe({
 Npm.depends({
   'fs-extra': '4.0.2',
   'request': '2.83.0',
-  'file-type': '7.1.0',
+  'file-type': '7.2.0',
   'eventemitter3': '2.0.3'
 });
 
@@ -17,7 +17,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.4');
   api.use('webapp', 'server');
   api.use(['reactive-var', 'tracker', 'http'], 'client');
-  api.use(['mongo', 'underscore', 'check', 'random', 'ecmascript', 'ostrio:cookies@2.2.3'], ['client', 'server']);
+  api.use(['mongo', 'underscore', 'check', 'random', 'ecmascript', 'ostrio:cookies@2.2.4'], ['client', 'server']);
   api.addAssets('worker.min.js', 'client');
   api.mainModule('server.js', 'server');
   api.mainModule('client.js', 'client');
