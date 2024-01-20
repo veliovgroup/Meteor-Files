9739d699a099a8cf19905206fe853da879b442c6
bratelefant <59534030+bratelefant@users.noreply.github.com>
2024-01-20 19:48:49 +0100

    add beta version

--- package.js
+++ package.js
@@ -7,7 +7,7 @@ Package.describe({
 });
 
 Package.onUse((api) => {
-  api.versionsFrom('2.14');
+  api.versionsFrom(['2.14', '3.0-beta.0']);
   api.use('webapp', 'server');
   api.use(['reactive-var', 'tracker', 'ddp-client'], 'client');
   api.use(['mongo', 'check', 'random', 'ecmascript', 'fetch', 'ostrio:cookies@2.7.2'], ['client', 'server']);
