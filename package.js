181fccac235655c5a82c43a41211762a3ed95a66
dr.dimitru <ceo@veliov.com>
2025-03-24 23:27:39 +0200

    📦 v3.0.0-beta.7

--- package.js
+++ package.js
@@ -1,35 +1,41 @@
 Package.describe({
   name: 'ostrio:files',
-  version: '3.0.0-beta.6',
+  version: '3.0.0-beta.7',
   summary: 'Upload files to a server or 3rd party storage: AWS:S3, GridFS, DropBox, and other',
   git: 'https://github.com/veliovgroup/Meteor-Files',
   documentation: 'README.md'
 });
 
 Package.onUse((api) => {
-  api.versionsFrom(['2.14', '3.0-beta.0']);
+  api.versionsFrom(['3.0.1']);
   api.use('webapp', 'server');
   api.use(['reactive-var', 'tracker', 'ddp-client'], 'client');
-  api.use(['mongo', 'check', 'random', 'ecmascript', 'fetch', 'ostrio:cookies@2.7.2'], ['client', 'server']);
+  api.use(['mongo', 'check', 'random', 'ecmascript', 'fetch', 'ostrio:cookies@2.9.0'], ['client', 'server']);
   api.addAssets('worker.min.js', 'client');
   api.mainModule('server.js', 'server');
   api.mainModule('client.js', 'client');
   api.export('FilesCollection');
+
+  // TypeScript setup
+  api.use(['zodern:types@1.0.13', 'typescript'], ['client', 'server'], { weak: true });
+  // For zodern:types to pick up our published types.
+  api.addAssets('index.d.ts', ['client', 'server']);
+
+  Npm.depends({
+    eventemitter3: '5.0.1',
+  });
 });
 
 Package.onTest((api) => {
   api.use('tinytest');
-  api.use('meteortesting:mocha');
+  api.use('meteortesting:mocha@3.2.0');
   api.use(['ecmascript', 'ostrio:files'], ['client', 'server']);
   api.addFiles('tests/helpers.js', ['client', 'server']);
   api.mainModule('tests/server.js', 'server');
-});
 
-Npm.depends({
-  'chai': '4.2.0',
-  'sinon': '7.5.0',
-  'diff': '4.0.1',
-  'fs-extra': '8.1.0',
-  eventemitter3: '4.0.7',
-  'abort-controller': '3.0.0'
+  Npm.depends({
+    eventemitter3: '5.0.1',
+    chai: '4.5.0',
+    sinon: '7.5.0',
+  });
 });
