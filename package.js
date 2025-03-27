1900ecc067e11d65e1ad2a791d341c839cf25c1f
dr.dimitru <ceo@veliov.com>
2025-03-28 00:44:53 +0200

    📦 v3.0.0-RC.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:files',
-  version: '3.0.0-rc.2',
+  version: '3.0.0-rc.3',
   summary: 'Upload files to a server or 3rd party storage: AWS:S3, GridFS, DropBox, and other',
   git: 'https://github.com/veliovgroup/Meteor-Files',
   documentation: 'README.md'
