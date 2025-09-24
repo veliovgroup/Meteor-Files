0fed32a86876a6a6937e725e641b61b109ef09c0
dr.dimitru <ceo@veliov.com>
2025-09-24 17:49:50 +0300

    📦 v3.0.1
    - 🔧 Fix FileObject data in upload events via `http` transport
    - 📔 Harden documentation

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'ostrio:files',
-  version: '3.0.0',
+  version: '3.0.1',
   summary: 'Upload files to a server or 3rd party storage: AWS:S3, GridFS, DropBox, and other',
   git: 'https://github.com/veliovgroup/Meteor-Files',
   documentation: 'README.md'
